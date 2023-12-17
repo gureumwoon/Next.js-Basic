323f8c0551db7c6d08e66944e0e0f34bb7ec66bb
gureumwoon <vfrvfr3221@gmail.com>
2023-12-17 17:21:45 +0900

    서버 컴포넌트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import os from 'os'; // 노드 APIs
 
 export default function Home() {
+  console.log("안녕!");
+  console.log(os.hostname());
   return <h1>홈페이지다!!</h1>
 }
