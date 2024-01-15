0379ee1f9683cea61c3d8dd0dc064cdb5efed8ae
gureumwoon <vfrvfr3221@gmail.com>
2024-01-15 20:21:20 +0900

    Next에서의 이미지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import os from 'os'; // 노드 APIs
 import Counter from '@/components/Counter';
+import Image from 'next/image';
 
 export default function Home() {
   console.log("안녕! - 서버");
@@ -8,6 +9,12 @@ export default function Home() {
     <>
       <h1>홈페이지다!!</h1>
       <Counter />
+      <Image
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+        alt="shop"
+        width={400}
+        height={400}
+      />
     </>
   )
 }
