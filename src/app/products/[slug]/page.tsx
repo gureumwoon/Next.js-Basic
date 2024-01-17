9d3fb598d55b595869e88d7dad36ee50af28ddb6
gureumwoon <vfrvfr3221@gmail.com>
2024-01-17 19:29:33 +0900

    함수명 수정

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -38,7 +38,7 @@ export default async function ProductPage({ params: { slug } }: Props) {
     )
 }
 
-export async function generateStaticParams() {
+export async function getStaticPaths() {
     // 모든 제품의 페이지들을 미리 만들어 둘 수 있게 해줄거임 (SSG)
     const products = await getProducts();
     return products.map(product => ({
